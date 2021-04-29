361722e0b0d2550ccae5b617562a197d4ecd74c7
James Bissick <bissickj@gmail.com>
2021-04-29 16:39:22 +0200

    :see_no_evil:  Update .gitignore file

---
+++ main.py
@@ -0,0 +1,44 @@
+# Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+
+# Specialized Data Types
+None
+
+
+print(2 + 3)
+
+# Print types of
+print(type(2 - 4))
+print(type(2 / 5))
+
+# Keeps the decimal anyways
+print(8.9 + 1.1)
+
+# Double operations return integers
+print(2 ** 2)
+print(5 // 4)
+
+# Modulo
+print(6 % 4)
+
+# Math functions
+print(round(3.2))
+print(abs(-21))
+
+# Operator precedence
+print(20 - 3 * 4)
+print((20 - 3) * 4)
+
+# Binary
+print(bin(5))
+print(int('0b101', 2))
